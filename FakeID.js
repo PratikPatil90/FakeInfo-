923301ed0283a12d544e333381d6bf6eb3be0e59
PratikPatil90 <161045591+PratikPatil90@users.noreply.github.com>
2024-08-13 17:15:43 +0530

    FakeID js file

--- FakeID.js
+++ FakeID.js
@@ -1,29 +1,3 @@
-// let box = document.querySelector("#Container");
-// let btn = document.querySelector("button");
-// let para = document.querySelector("p");
-
-// let url = "https://fakerapi.it/api/v1/persons?";
-
-// btn.addEventListener("click", async ()=>{
-//     // let para = document.querySelector("p");
-//     let finfo = await  getFakeInfo();
-//     console.log(finfo);
-//     show(finfo);
-// });
-
-// function show(finfo){
-//     para.innerText = `Name: ${finfo.firstname} ${finfo.lastname}\nEmail: ${finfo.email}\nPhone: ${finfo.phone}`;
-// }
-// async function getFakeInfo(){
-//     try{
-//         let res = await axios.get(url);
-//         console.log(res.data.data[0]);
-//     }
-//     catch(e)
-//     {
-//         console.log("Error :",e);
-//     }
-// }
 
 let box = document.querySelector("#Container");
 let btn = document.querySelector("button");
